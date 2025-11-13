8af5d42a518f4f795857e4750bb517def55cc653
George-Olufemi <georgeoluolufemi@gmail.com>
2025-11-13 14:51:55 +0100

    update: UI spinner

--- app/workerportal/page.tsx
+++ app/workerportal/page.tsx
@@ -581,7 +581,6 @@ const WorkersPage = () => {
               {acceptRejectMutation.isPending ||
               updateStatusMutation.isPending ? (
                 <div className="flex items-center gap-2">
-                  <div className="w-4 h-4 border-2 border-white border-t-transparent rounded-full animate-spin" />
                   <Spinner />
                 </div>
               ) : (
